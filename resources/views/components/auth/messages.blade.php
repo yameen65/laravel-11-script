9b21e3a5027425e34c838b60fbc77b9fc1c7559f
Inaam ul haq <waleedhaq339@gmail.com>
2024-09-16 17:47:27 +0500

    components for notifications and messages created

---
+++ resources/views/components/auth/messages.blade.php
@@ -0,0 +1,71 @@
+<li class="nav-item dropdown active">
+    <a class="nav-link dropdown-toggle position-relative" href="#" id="messagesDropdown" data-bs-toggle="dropdown">
+        <i class="align-middle fas fa-envelope-open"></i>
+    </a>
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="messagesDropdown">
+        <div class="dropdown-menu-header">
+            <div class="position-relative">
+                4 New Messages
+            </div>
+        </div>
+        <div class="list-group">
+            <a href="#" class="list-group-item">
+                <div class="row g-0 align-items-center">
+                    <div class="col-2">
+                        <img src="img/avatars/avatar-5.jpg" class="avatar img-fluid rounded-circle"
+                            alt="Michelle Bilodeau">
+                    </div>
+                    <div class="col-10 ps-2">
+                        <div class="text-dark">Michelle Bilodeau</div>
+                        <div class="text-muted small mt-1">Nam pretium turpis et arcu. Duis arcu
+                            tortor.</div>
+                        <div class="text-muted small mt-1">5m ago</div>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="list-group-item">
+                <div class="row g-0 align-items-center">
+                    <div class="col-2">
+                        <img src="img/avatars/avatar-3.jpg" class="avatar img-fluid rounded-circle" alt="Kathie Burton">
+                    </div>
+                    <div class="col-10 ps-2">
+                        <div class="text-dark">Kathie Burton</div>
+                        <div class="text-muted small mt-1">Pellentesque auctor neque nec urna.
+                        </div>
+                        <div class="text-muted small mt-1">30m ago</div>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="list-group-item">
+                <div class="row g-0 align-items-center">
+                    <div class="col-2">
+                        <img src="img/avatars/avatar-2.jpg" class="avatar img-fluid rounded-circle"
+                            alt="Alexander Groves">
+                    </div>
+                    <div class="col-10 ps-2">
+                        <div class="text-dark">Alexander Groves</div>
+                        <div class="text-muted small mt-1">Curabitur ligula sapien euismod
+                            vitae.</div>
+                        <div class="text-muted small mt-1">2h ago</div>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="list-group-item">
+                <div class="row g-0 align-items-center">
+                    <div class="col-2">
+                        <img src="img/avatars/avatar-4.jpg" class="avatar img-fluid rounded-circle" alt="Daisy Seger">
+                    </div>
+                    <div class="col-10 ps-2">
+                        <div class="text-dark">Daisy Seger</div>
+                        <div class="text-muted small mt-1">Aenean tellus metus, bibendum sed,
+                            posuere ac, mattis non.</div>
+                        <div class="text-muted small mt-1">5h ago</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="dropdown-menu-footer">
+            <a href="#" class="text-muted">Show all messages</a>
+        </div>
+    </div>
+</li>
