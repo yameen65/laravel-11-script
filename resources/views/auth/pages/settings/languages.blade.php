f9a71349d7513f0f8636731dc44bb64a324b6c6f
Inaam ul haq <waleedhaq339@gmail.com>
2024-10-16 15:08:21 +0500

    languages features

--- resources/views/auth/pages/settings/languages.blade.php
+++ resources/views/auth/pages/settings/languages.blade.php
@@ -1,5 +1,14 @@
 <x-settings title="Languages" sub-title="Change your system default language or add new languages for multilingual use">
-    <x-auth.card card-header="Languages" header-button="true">
+    <x-auth.card card-header="Languages" header-button="">
+        <x-slot:header-custom>
+            <select class="form-control" name="default_language" id="default_language">
+                <option value="" selected disabled>Select default language</option>
+                <option value="">ok</option>
+                <option value="">ok</option>
+                <option value="">ok</option>
+            </select>
+        </x-slot:header-custom>
+
 
     </x-auth.card>
 </x-settings>
